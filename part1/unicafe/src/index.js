3677a7a3bcf17b97ba29d1f9f262b360a19268f4
Ynjxsjmh <ynjxsjmh@gmail.com>
2020-08-02 18:31:30 +0800

    Finish 1.6: unicafe step1
    Implement a web application for collecting customer feedback.
    There are only three options for feedback: _good_,
    _neutral_, and _bad_.
    
    The application must display the total number of collected feedback
    for each category.
    
    Note that your application needs to work only during a single browser
    session. Once you refresh the page, the collected feedback is allowed
    to disappear.

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,15 +1,56 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
+const Button = (props) => (
+  <button onClick={props.handleClick}>
+    {props.text}
+  </button>
+);
+
+const Feedback = (props) => {
+  return (
+    <>
+      <h1>give feedback</h1>
+      <Button handleClick={props.handleGoodClick}    text="good" />
+      <Button handleClick={props.handleNeutralClick} text="neutral" />
+      <Button handleClick={props.handleBadClick}     text="bad" />
+    </>
+  );
+};
+
+const Statistic = (props) => {
+  return (
+    <div>
+      <h1>statistic</h1>
+      <div>good {props.good}</div>
+      <div>neutral {props.neutral}</div>
+      <div>bad {props.bad}</div>
+    </div>
+  );
+};
+
 const App = () => {
   // save clicks of each button to own state
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
+  const handleGoodClick = () => {
+    setGood(good + 1);
+  };
+
+  const handleNeutralClick = () => {
+    setNeutral(neutral + 1);
+  };
+
+  const handleBadClick = () => {
+    setBad(bad + 1);
+  };
+
   return (
     <div>
-      code here
+      <Feedback handleGoodClick={handleGoodClick} handleNeutralClick={handleNeutralClick} handleBadClick={handleBadClick} />
+      <Statistic good={good} neutral={neutral} bad={bad} />
     </div>
   );
 };
