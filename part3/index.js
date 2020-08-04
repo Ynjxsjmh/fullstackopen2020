6fead29b83ea8396202f200a1d4d9b7dbbde2d0a
Ynjxsjmh <ynjxsjmh@gmail.com>
2020-08-04 19:10:25 +0800

    Finish 3.9 phonebook backend step9
    Make the backend work with the frontend from the previous part.
    
    You will probably have to do some small changes to the frontend, at
    least to the URLs for the backend.
    
    Remember to keep the developer console open in your browser.
    If some HTTP requests fail, you should check from the _Network_-tab
    what is going on.
    
    Keep an eye on the backend's console as well. If you did not do
    the previous exercise, it is worth it to print the request data
    or _request.body_ to the console in the event handler responsible for
    POST requests.

--- part3/index.js
+++ part3/index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const morgan = require('morgan');
-const app = express();
+const cors = require('cors');
 
 morgan.token("body", req => JSON.stringify(req.body));
 
+const app = express();
+
 app.use(express.json());
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
+app.use(cors());
 
 
 let persons = [
