b1c7c655f044a8b48c0c8a7320934fe33fc579fa
Ynjxsjmh <ynjxsjmh@gmail.com>
2020-08-05 11:43:49 +0800

    Finish 3.13: Phonebook database, step1
    Change the fetching of all phonebook entries so that the data is
    _fetched from the database_.
    
    Verify that the frontend works after the changes have been made.
    
    Write all Mongoose-specific code into its own module, just like we did
    in the chapter [Database configuration into its own
    module](https://fullstackopen.com/en/part3/saving_data_to_mongo_db#database-configuration-into-its-own-module).

--- part3/index.js
+++ part3/index.js
@@ -1,6 +1,8 @@
+require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
+const Person = require('./models/person');
 
 morgan.token("body", req => JSON.stringify(req.body));
 
@@ -12,47 +14,25 @@ app.use(cors());
 app.use(express.static('build'));
 
 
-let persons = [
-  {
-    "name": "Arto Hellas",
-    "number": "040-123456",
-    "id": 1
-  },
-  {
-    "name": "Ada Lovelace",
-    "number": "39-44-5323523",
-    "id": 2
-  },
-  {
-    "name": "Dan Abramov",
-    "number": "12-43-234345",
-    "id": 3
-  },
-  {
-    "name": "Mary Poppendieck",
-    "number": "39-23-6423122",
-    "id": 4
-  }
-];
-
 app.get('/info', (req, res) => {
-  let date = new Date();
-  res.send(`<p>Phonebook has info for ${persons.length} people</p>` + date);
+  Person.find({}).then(persons => {
+    let date = new Date();
+    res.send(`<p>Phonebook has info for ${persons.length} people</p>` + date);
+  });
 });
 
 app.get('/api/persons', (req, res) => {
-  res.json(persons);
+  Person.find({}).then(persons => {
+    res.json(persons);
+  });
 });
 
 app.get('/api/persons/:id', (req, res) => {
   const id = Number(req.params.id);
-  const person = persons.find(p => p.id === id);
 
-  if (person) {
+  Person.findById(id).then(person => {
     res.json(person);
-  } else {
-    res.status(404).end();
-  }
+  });
 });
 
 app.delete('/api/persons/:id', (req, res) => {
@@ -88,7 +68,7 @@ app.post('/api/persons', (req, res) => {
   res.json(person);
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
