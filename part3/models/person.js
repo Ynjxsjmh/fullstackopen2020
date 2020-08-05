451531ba5ebd1f5d7ea04098e228ba421614b29e
Ynjxsjmh <ynjxsjmh@gmail.com>
2020-08-05 16:07:17 +0800

    Finish 3.19: Phonebook database, step7
    Add validation to your phonebook application, that will make sure that
    a newly added person has a unique name. Our current frontend won't
    allow users to try and create duplicates, but we can attempt to create
    them directly with Postman or the VS Code REST client.
    
    Mongoose does not offer a built-in validator for this purpose. Install
    the [mongoose-unique-validator](https://github.com/blakehaswell/mongoose-unique-validator#readme)
    package with npm and use it instead.
    
    If an HTTP POST request tries to add a name that is already in the
    phonebook, the server must respond with an appropriate status code and
    error message.

--- part3/models/person.js
+++ part3/models/person.js
@@ -1,4 +1,9 @@
 const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
+
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
 
 const url = process.env.MONGODB_URI;
 
@@ -13,8 +18,16 @@ mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
   });
 
 const personSchema = new mongoose.Schema({
-  name: String,
-  number: String,
+  name: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  number: {
+    type: String,
+    required: true,
+    unique: true
+  }
 });
 
 personSchema.set('toJSON', {
