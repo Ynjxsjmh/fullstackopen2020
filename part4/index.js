3a9289080d291f11b4be0ae698cbfb4455b38b20
Ynjxsjmh <ynjxsjmh@gmail.com>
2020-08-05 21:11:20 +0800

    4.0 cont.: app.js creates the actual application
    The _index.js_ file only imports the actual application from the
    _app.js_ file and then starts the application.
    
    The _app.js_ file that creates the actual application, takes the
    router into use.

--- part4/index.js
+++ part4/index.js
@@ -1,18 +1,10 @@
+const app = require('./app'); // the actual Express application
 const http = require('http');
-const express = require('express');
-const app = express();
-const cors = require('cors');
-const mongoose = require('mongoose');
 const config = require('./utils/config');
 const logger = require('./utils/logger');
-const blogsRouter = require('./controllers/blogs');
 
-mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+const server = http.createServer(app);
 
-app.use(cors());
-app.use(express.json());
-app.use('/api/blogs', blogsRouter);
-
-app.listen(config.PORT, () => {
+server.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`);
 });
