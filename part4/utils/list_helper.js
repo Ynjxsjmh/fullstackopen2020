30740593875a20fef34a16f839907f61ca5371ef
Ynjxsjmh <ynjxsjmh@gmail.com>
2020-08-06 22:03:44 +0800

    Finish 4.6*: helper functions and unit tests, step4
    Define a function called _mostBlogs_ that receives an array of blogs
    as a parameter. The function returns the _author_ who has the largest
    amount of blogs.
    
    If there are many top bloggers, then it is enough to return any one of them.

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -17,8 +17,33 @@ const favoriteBlog = (blogs) => {
   return (({ title, likes, author }) => ({ title, likes, author }))(blog);
 };
 
+const mostBlogs = (blogs) => {
+  if (blogs.length === 0) {
+    return {};
+  }
+
+  let arr = new Array();
+
+  for (let i = 0; i < blogs.length; i++) {
+    arr[blogs[i].author] = (arr[blogs[i].author] || 0) + 1;
+  }
+
+  let min = -1;
+  let blog;
+
+  Object.keys(arr).forEach(function(key) {
+    if (arr[key] > min) {
+      blog = {author: key, blogs: arr[key]};
+      min = arr[key];
+    }
+  });
+
+  return blog;
+};
+
 module.exports = {
   dummy,
   totalLikes,
-  favoriteBlog
+  favoriteBlog,
+  mostBlogs
 };
