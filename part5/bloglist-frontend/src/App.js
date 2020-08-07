47ef6954ffa2cc368a4608940106e87cf758cd33
Ynjxsjmh <ynjxsjmh@gmail.com>
2020-08-08 00:42:24 +0800

    Finish 5.2: bloglist frontend, step2
    Implement a way to log out.
    
    Log out with the command:
    
    ```js
    window.localStorage.removeItem('loggedNoteappUser')
    ```
    
    or with the command which empties _localstorage_ completely:
    
    ```js
    window.localStorage.clear()
    ```

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -42,6 +42,13 @@ const App = () => {
     }
   };
 
+  const handleLogout = (event) => {
+    event.preventDefault();
+
+    window.localStorage.removeItem('loggedBlogappUser');
+    setUser(null);
+  };
+
   const loginForm = () => (
     <>
       <h2>Log in to application</h2>
@@ -72,7 +79,9 @@ const App = () => {
   const blogForm = () => (
     <>
       <h2>blogs</h2>
-      <p>{user.name} logged-in</p>
+      <p>{user.name} logged-in
+        <button onClick={handleLogout}>logout</button>
+      </p>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
