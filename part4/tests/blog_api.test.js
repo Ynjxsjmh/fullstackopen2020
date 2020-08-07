97631e8abbe8f85992886aaf9bf689fb69bb1dac
Ynjxsjmh <ynjxsjmh@gmail.com>
2020-08-07 12:58:46 +0800

    4.13 cont.: add the checking the blogs stored in the database function into a new file
    Both tests check the state stored in the database after the saving
    operation, by fetching all the blogs of the application.
    
    ```js
    const response = await api.get('/api/blogs')
    ```
    
    The same verification steps will repeat in other tests later on, and
    it is a good idea to extract these steps into helper functions.
    
    Add the function into a new file called _tests/test_helper.js_ that is
    in the same directory as the test file.

--- part4/tests/blog_api.test.js
+++ part4/tests/blog_api.test.js
@@ -1,33 +1,18 @@
 const mongoose = require('mongoose');
 const supertest = require('supertest');
 const app = require('../app');
-
+const helper = require('./test_helper');
 const api = supertest(app);
 const Blog = require('../models/blog');
 
 
-const initialBlogs = [
-  {
-    title: "React patterns",
-    author: "Michael Chan",
-    url: "https://reactpatterns.com/",
-    likes: 7,
-  },
-  {
-    title: "Go To Statement Considered Harmful",
-    author: "Edsger W. Dijkstra",
-    url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
-    likes: 5,
-  },
-];
-
 beforeEach(async () => {
   await Blog.deleteMany({});
 
-  let blogObject = new Blog(initialBlogs[0]);
+  let blogObject = new Blog(helper.initialBlogs[0]);
   await blogObject.save();
 
-  blogObject = new Blog(initialBlogs[1]);
+  blogObject = new Blog(helper.initialBlogs[1]);
   await blogObject.save();
 });
 
@@ -41,7 +26,7 @@ test('blogs are returned as json', async () => {
 test('all blogs are returned', async () => {
   const response = await api.get('/api/blogs');
 
-  expect(response.body).toHaveLength(initialBlogs.length);
+  expect(response.body).toHaveLength(helper.initialBlogs.length);
 });
 
 test('the unique identifier property of the blog posts is named "id"', async () => {
@@ -66,11 +51,10 @@ test('a valid blog can be added', async () => {
     .expect(201)
     .expect('Content-Type', /application\/json/);
 
-  const response = await api.get('/api/blogs');
+  const blogsAtEnd = await helper.blogsInDb();
+  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1);
 
-  const titles = response.body.map(r => r.title);
-
-  expect(response.body).toHaveLength(initialBlogs.length + 1);
+  const titles = blogsAtEnd.map(r => r.title);
   expect(titles).toContain(
     'Canonical string reduction'
   );
