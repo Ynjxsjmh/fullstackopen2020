09234020b8cb62266e13bc4eb17ddfc61024c9e1
Ynjxsjmh <ynjxsjmh@gmail.com>
2020-08-07 17:46:02 +0800

    Finish 4.17: bloglist expansion, step5
    - Modify listing all blogs so that the creator's user information is
      displayed with the blog.
    - and listing all users also displays the blogs created by each user.
    
    Post http://localhost:3003/api/users to create new user
    
    ```
    {
      "username": "Hessu",
      "name": "Edsger W. Dijkstra",
      "password": "fsdfds"
    }
    ```
    
    Copy the returned id
    
    Post http://localhost:3003/api/blogs to create new post
    
    ```
    {
      "title": "React patterns",
      "author": "Michael Chan",
      "url": "https://reactpatterns.com/",
      "likes": 7,
      "userId": "5f2d10cae488ea1358050873"
    }
    ```
    
    Access http://localhost:3003/api/blogs, if it shows:
    
    ```
    {
      title: "sdsf",
      author: "Michael Chan",
      url: "https://example.com/",
      likes: 7,
      user: {
        username: "Hessu",
        id: "5f2d10cae488ea1358050873"
      },
      id: "5f2d1a1b602de70ac4a80198"
    }
    ```
    
    Access http://localhost:3003/api/users, if it shows:
    
    ```
    {
      blogs: [
        {
          title: "sdsf",
          author: "Michael Chan",
          url: "https://example.com/",
          id: "5f2d1a1b602de70ac4a80198"
        }
      ],
      username: "Hessu",
      id: "5f2d10cae488ea1358050873"
    }
    ```

--- part4/controllers/users.js
+++ part4/controllers/users.js
@@ -32,7 +32,9 @@ usersRouter.post('/', async (request, response, next) => {
 });
 
 usersRouter.get('/', async (request, response) => {
-  const users = await User.find({});
+  const users = await User
+    .find({}).populate('blogs', { author: 1, title: 1, url: 1 });
+
   response.json(users);
 });
 
