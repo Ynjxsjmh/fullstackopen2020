68c1a049696217bbf408907483328ba16a4aa7e8
Ynjxsjmh <ynjxsjmh@gmail.com>
2020-08-07 15:28:26 +0800

    Finish 4.15: bloglist expansion, step3
    Implement a way to see the details of all users by doing a suitable
    HTTP request.

--- part4/controllers/users.js
+++ part4/controllers/users.js
@@ -23,4 +23,9 @@ usersRouter.post('/', async (request, response) => {
 
 });
 
+usersRouter.get('/', async (request, response) => {
+  const users = await User.find({});
+  response.json(users);
+});
+
 module.exports = usersRouter;
