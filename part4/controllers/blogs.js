836eba7196d68f13750335151bb48bf4cc0229f9
Ynjxsjmh <ynjxsjmh@gmail.com>
2020-08-07 18:44:32 +0800

    Finish 4.19: bloglist expansion, step7
    Modify adding new blogs so that it is only possible if a valid token
    is sent with the HTTP POST request. The user identified by the token
    is designated as the creator of the blog.
    
    Post http://localhost:3003/api/blogs
    
    ```
    Add below to the header
    
    Key           | Value
    Authorization | bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6IjEyMyIsImlkIjoiNWYyZDI4MWJhYmFiMDcyMGY0MThmNzA2IiwiaWF0IjoxNTk2Nzk2OTcyfQ.zCL-xYdX0pBCQtRUpsFHsO9w04nfgPxEfU4glVOXXXo
    
    {
        "title": "Authorization",
        "url": "www.authorization.com"
    }
    ```
    
    Check if it returns
    
    ```
    {
        "title": "Authorization",
        "author": "123",
        "url": "www.authorization.com",
        "likes": 0,
        "user": "5f2d281babab0720f418f706",
        "id": "5f2d3048fb2623169867d71b"
    }
    ```

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -1,6 +1,17 @@
 const blogsRouter = require('express').Router();
 const Blog = require('../models/blog');
 const User = require('../models/user');
+const jwt = require('jsonwebtoken');
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization');
+
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    const token = authorization.split(' ')[1];
+    return token;
+  }
+  return null;
+};
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog
@@ -16,11 +27,17 @@ blogsRouter.post('/', async (request, response) => {
     return response.status(400).end();
   }
 
-  const user = await User.findById(body.userId);
+  const token = getTokenFrom(request);
+  const decodedToken = jwt.verify(token, process.env.SECRET);
+  if (!token || !decodedToken.id) {
+    return response.status(401).json({ error: 'token missing or invalid' });
+  }
+
+  const user = await User.findById(decodedToken.id);
 
   const blog = new Blog({
     title: body.title,
-    author: body.author,
+    author: user.username,
     url: body.url,
     likes: body.likes || 0,
     user: user._id,
