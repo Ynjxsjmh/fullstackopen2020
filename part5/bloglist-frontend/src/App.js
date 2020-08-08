8a6ac7a090b225626b20eef640a547621f9bf8be
Ynjxsjmh <ynjxsjmh@gmail.com>
2020-08-08 22:16:54 +0800

    Finish 5.9*: Blog list frontend, step9
    Modify the application to list the blog posts by the number of
    _likes_. Sorting the blog posts can be done with the array
    [sort](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort)
    method.

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -114,9 +114,11 @@ const App = () => {
 
   const blogForm = () => (
     <>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} setUpdate={setUpdate} />
-      )}
+      {blogs
+       .sort((a, b) => b.likes - a.likes)
+       .map(blog =>
+         <Blog key={blog.id} blog={blog} setUpdate={setUpdate} />
+       )}
     </>
   );
 
