193105107e6752e152d73dbccc048ec41cb20cb2
Ynjxsjmh <ynjxsjmh@gmail.com>
2020-08-11 21:42:21 +0800

    Finish 6.14 Anecdotes and the backend, step2
    Modify the creation of new anecdotes, such that the anecdotes are
    stored in the backend.
    
    Because the backend generates ids for the anecdotes, we'll change the
    action creator _createAnecdote_.

--- part6/redux-anecdotes/src/components/AnecdoteForm.js
+++ part6/redux-anecdotes/src/components/AnecdoteForm.js
@@ -3,15 +3,19 @@ import { useDispatch } from 'react-redux';
 import { createAnecdote  } from '../reducers/anecdoteReducer';
 import { createNotification, deleteNotification } from '../reducers/notificationReducer';
 
+import anecdoteService from '../services/anecdotes';
+
 const AnecdoteForm = (props) => {
   const dispatch = useDispatch();
 
-  const addAnecdote = (event) => {
+  const addAnecdote = async (event) => {
     event.preventDefault();
     const content = event.target.anecdote.value;
     event.target.anecdote.value = '';
 
-    dispatch(createAnecdote(content));
+    const newAnecdote = await anecdoteService.createNew(content);
+    dispatch(createAnecdote(newAnecdote));
+
     dispatch(createNotification(`You added anecdote '${content}'`));
     setTimeout(() => {
       dispatch(deleteNotification());
--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,6 +1,4 @@
 
-const getId = () => (100000 * Math.random()).toFixed(0);
-
 const reducer = (state = [], action) => {
   console.log('state now: ', state);
   console.log('action', action);
@@ -29,14 +27,10 @@ const reducer = (state = [], action) => {
   return state;
 };
 
-export const createAnecdote = (content) => {
+export const createAnecdote = (data) => {
   return {
     type: 'NEW_ANECDOTE',
-    data: {
-      content,
-      id: getId(),
-      votes: 0
-    }
+    data
   };
 };
 
