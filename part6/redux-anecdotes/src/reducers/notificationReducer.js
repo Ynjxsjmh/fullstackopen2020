55c4b49c83032e27858caf9a496fae6bfa80dc9f
Ynjxsjmh <ynjxsjmh@gmail.com>
2020-08-12 00:31:46 +0800

    Finish 6.21 anecdotes, the grand finale
    You (probably) have one nasty bug in your application. If the user
    clicks the vote button multiple times in a row, the notification is
    displayed funnily. For example if a user votes twice in three seconds,
    the last notification is only displayed for two seconds (assuming the
    notification is normally shown for 5 seconds). This happens because
    removing the first notification accidentally removes the second
    notification.
    
    Fix the bug so that after multiple votes in a row, the notification
    for the last vote is displayed for five seconds.
    
    This can be done by cancelling the removal of the previous
    notification when a new notification is displayed whenever necessary.
    
    The
    [documentation](https://developer.mozilla.org/en-US/docs/Web/API/WindowOrWorkerGlobalScope/setTimeout)
    for the setTimeout function might also be useful for this.

--- part6/redux-anecdotes/src/reducers/notificationReducer.js
+++ part6/redux-anecdotes/src/reducers/notificationReducer.js
@@ -2,13 +2,14 @@
 export const createNotification = (content, delay=5) => {
   return async dispatch => {
     dispatch({
-      type: 'SET_NOTIFICATION',
-      data: content
+      type: "SET_NOTIFICATION",
+      data: {
+        content,
+        delay: setTimeout(() => {
+          dispatch(deleteNotification(""));
+        }, delay * 1000),
+      },
     });
-
-    setTimeout(() => {
-      dispatch(deleteNotification(""));
-    }, delay * 1000);
   };
 };
 
@@ -21,7 +22,8 @@ export const deleteNotification = () => {
 const notificationReducer = (state = "", action) => {
   switch (action.type) {
     case 'SET_NOTIFICATION':
-      return action.data;
+      clearTimeout(action.data.delay);
+      return action.data.content;
     case 'DELETE_NOTIFICATION':
       return "";
     default:
