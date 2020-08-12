3b644f61fc6b5b2d3ce3e47654c29f21b28e21e6
Ynjxsjmh <ynjxsjmh@gmail.com>
2020-08-12 13:17:32 +0800

    Finish 7.4: anecdotes and hooks step1
    Simplify the anecdote creation form of your application with the
    _useField_ custom hook we defined earlier.
    
    One natural place to save the custom hooks of your application is in
    the _/src/hooks/index.js_ file.

--- part7/routed-anecdotes/src/App.js
+++ part7/routed-anecdotes/src/App.js
@@ -4,6 +4,7 @@ import {
   useParams, useHistory
 } from "react-router-dom";
 import React, { useState } from 'react';
+import  { useField } from './hooks';
 
 const Menu = () => {
   const padding = {
@@ -63,17 +64,17 @@ const Footer = () => (
 );
 
 const CreateNew = (props) => {
-  const [content, setContent] = useState('');
-  const [author, setAuthor] = useState('');
-  const [info, setInfo] = useState('');
+  const content = useField('text');
+  const author = useField('text');
+  const info = useField('text');
   const history = useHistory();
 
   const handleSubmit = (e) => {
     e.preventDefault();
     props.addNew({
-      content,
-      author,
-      info,
+      content: content.value,
+      author: author.value,
+      info: info.value,
       votes: 0
     });
     history.push('/');
@@ -85,15 +86,15 @@ const CreateNew = (props) => {
       <form onSubmit={handleSubmit}>
         <div>
           content
-          <input name='content' value={content} onChange={(e) => setContent(e.target.value)} />
+          <input name='content' {...content} />
         </div>
         <div>
           author
-          <input name='author' value={author} onChange={(e) => setAuthor(e.target.value)} />
+          <input name='author' {...author} />
         </div>
         <div>
           url for more info
-          <input name='info' value={info} onChange={(e)=> setInfo(e.target.value)} />
+          <input name='info' {...info} />
         </div>
         <button>create</button>
       </form>
