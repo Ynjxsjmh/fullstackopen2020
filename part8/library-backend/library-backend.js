36e096949ad09415761c491565ddac4aa53e5448
Ynjxsjmh <ynjxsjmh@gmail.com>
2020-08-13 10:29:20 +0800

    Finish 8.1: The number of books and authors
    Implement queries _bookCount_ and _authorCount_ which return the
    number of books and the number of authors.
    
    The query
    
    ```gql
    query {
      bookCount
      authorCount
    }
    ```
    
    should return
    
    ```json
    {
      "data": {
        "bookCount": 7,
        "authorCount": 5
      }
    }
    ```
    
    ==========
    
    When Apollo-server is run on development mode (_node filename.js_), it
    starts a
    [GraphQL-playground](https://www.apollographql.com/docs/apollo-server/testing/graphql-playground/)
    to address http://localhost:4000/graphql. This is very useful for a
    developer, and can be used to make queries to the server.

--- part8/library-backend/library-backend.js
+++ part8/library-backend/library-backend.js
@@ -84,12 +84,30 @@ let books = [
 ];
 
 const typeDefs = gql`
+  type Author {
+    name: String!
+    id: ID!
+    born: Int!,
+  }
+
+  type Book {
+    title: String!
+    published: Int!,
+    author: String!
+    id: ID!
+    genres: [String]!
+  }
+
   type Query {
+    authorCount: Int!
+    bookCount: Int!,
   }
 `;
 
 const resolvers = {
   Query: {
+    authorCount: () => authors.length,
+    bookCount: () => books.length,
   }
 };
 
