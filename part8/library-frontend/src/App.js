3a194f797a9f2d34195b92348015aff66951171d
Ynjxsjmh <ynjxsjmh@gmail.com>
2020-08-15 01:55:29 +0800

    8.18 cont.: Implement the funtion to login
    Add variable _token_ to the application's state. It will contain
    user's token when a is logged in. If _token_ is undefined, we render
    the _LoginForm_-component responsible for user login. The component
    receives an error handler and the _setToken_-function as parameters.
    
    Next we define a mutation for logging in
    
    ```js
    export const LOGIN = gql`
      mutation login($username: String!, $password: String!) {
        login(username: $username, password: $password)  {
          value
        }
      }
    `
    ```
    
    The _LoginForm_-component works pretty much just like all other
    components doing mutations we have previously created.
    
    We are using an effect hook again. Here it's used to save the token's
    value to the state of the _App_ component and the local storage after
    the server has responded to the mutation.
    
    Use of the effect hook is necessary to avoid an endless rendering
    loop.

--- part8/library-frontend/src/App.js
+++ part8/library-frontend/src/App.js
@@ -2,9 +2,11 @@ import React, { useState } from 'react';
 import Authors from './components/Authors';
 import Books from './components/Books';
 import NewBook from './components/NewBook';
+import LoginForm from './components/LoginForm';
 
 
 const App = () => {
+  const [token, setToken] = useState(null);
   const [page, setPage] = useState('authors');
 
   return (
@@ -12,7 +14,15 @@ const App = () => {
       <div>
         <button onClick={() => setPage('authors')}>authors</button>
         <button onClick={() => setPage('books')}>books</button>
-        <button onClick={() => setPage('add')}>add book</button>
+
+        {token ? (
+          <>
+            <button onClick={() => setPage('add')}>add book</button>
+            <button>logout</button>
+          </>
+        ) : (
+          <button onClick={() => setPage('login')}>login</button>
+        )}
       </div>
 
       <Authors
@@ -25,6 +35,11 @@ const App = () => {
 
       <NewBook
         show={page === 'add'}
+      />
+
+      <LoginForm
+        show={page === 'login'}
+        setToken={setToken}
       />
 
     </div>
