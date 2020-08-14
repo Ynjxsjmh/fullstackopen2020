b34a41cf279977d33ee82e299defb9d54e222297
Ynjxsjmh <ynjxsjmh@gmail.com>
2020-08-14 18:39:18 +0800

    8.10 cont.: separate the query definitions into queries.js
    At the moment in our code queries and component are defined in the
    same place.
    
    Separate the query definitions into their own file _queries.js_.

--- part8/library-frontend/src/components/NewBook.js
+++ part8/library-frontend/src/components/NewBook.js
@@ -1,21 +1,8 @@
 import React, { useState } from 'react';
 
-import { gql, useMutation } from '@apollo/client';
+import { useMutation } from '@apollo/client';
 
-
-const CREATE_BOOK = gql`
-mutation createBook($title: String!, $author: String!, $published: Int!, $genres: [String]!) {
-  addBook(
-    title: $title,
-    author: $author,
-    published: $published,
-    genres: $genres
-  ) {
-    title,
-    author
-  }
-}
-`;
+import { CREATE_BOOK } from '../queries';
 
 const NewBook = (props) => {
   const [title, setTitle] = useState('');
